0feb8948fb322d1d7d5a8de9a1b56629d39a8473
Kent Fredric <kentfredric@gmail.com>
2014-04-11 08:49:58 +1200

    Better abstract/description in Transactional.pm

--- lib/Activity/Transactional.pm
+++ lib/Activity/Transactional.pm
@@ -3,8 +3,14 @@ package Activity::Transactional;
 use strict;
 use warnings;
 
-# ABSTRACT: A module by JJL
+# ABSTRACT: A Transaction-Oriented Programming work-flow toolkit
 
 # AUTHORITY
+
+=head1 SYNOPSIS
+
+This code is still under heavy development.
+
+=cut
 
 1;
