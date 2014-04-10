0c96cf2272d1bab2b68ba5659735993e39f35947
Kent Fredric <kentfredric@gmail.com>
2014-04-11 07:33:50 +1200

    Define the TransactionList type as ArrayRef[ Transaction ]

--- lib/Activity/Transactional/Types.pm
+++ lib/Activity/Transactional/Types.pm
@@ -5,6 +5,8 @@ package Activity::Transactional::Types;
 use MooseX::Types -declare => [
   qw(Activity Transaction TransactionList)
 ];
+use MooseX::Types::Moose qw( ArrayRef );
 
 class_type Transaction, {class => 'Activity::Transactional::Transaction'};
 role_type Activity, {role => 'Activity::Transactional::Activity'};
+subtype TransactionList, as ArrayRef[ Transaction ];
