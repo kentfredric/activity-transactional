8d1ccb9ef50744b4c6784bf6b0329180bdf59fb6
Kent Fredric <kentfredric@gmail.com>
2014-04-11 08:22:51 +1200

    Document CodeRef.pm

--- lib/Activity/Transactional/Activity/CodeRef.pm
+++ lib/Activity/Transactional/Activity/CodeRef.pm
@@ -1,10 +1,25 @@
 package Activity::Transactional::Activity::CodeRef;
 
+# ABSTRACT: A simple CodeRef based Activity.
+
 # AUTHORITY
 
 use Moose;
 
+=head1 SYNOPSIS
+
+  my $activity = Activity::Transactional::Activity::CodeRef->new(
+    commit_code   => sub {},
+    rollback_code => sub {},
+  );
+
+=cut
+
 with 'Activity::Transactional::Activity';
+
+=attr C<commit_code>
+
+=cut
 
 has commit_code => (
   isa      => 'CodeRef',
@@ -12,16 +27,28 @@ has commit_code => (
   required => 1,
 );
 
+=attr C<rollback_code>
+
+=cut
+
 has rollback_code => (
   isa      => 'CodeRef',
   is       => 'ro',
   required => 1,
 );
 
+=method C<commit>
+
+=cut
+
 sub commit {
   my ($self) = @_;
   $self->commit_code->();
 }
+
+=method C<rollback>
+
+=cut
 
 sub rollback {
   my ($self) = @_;
