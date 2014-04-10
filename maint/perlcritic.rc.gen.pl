90b05613f072e9006fb9fd6f7ee9d02bd7ec5f8c
Kent Fredric <kentfredric@gmail.com>
2014-04-11 07:55:20 +1200

    Build results of 460f630 (on master)

--- maint/perlcritic.rc.gen.pl
+++ maint/perlcritic.rc.gen.pl
@@ -27,6 +27,12 @@ for my $wordlist (@stopwords) {
 }
 $bundle->remove_policy('CodeLayout::RequireUseUTF8');
 $bundle->remove_policy('Modules::RequirePerlVersion');
+$bundle->remove_policy('Compatibility::PerlMinimumVersionAndWhy');
+$bundle->remove_policy('Compatibility::RequireFinalReturn');
+$bundle->remove_policy('Subroutines::ProhibitCallsToUndeclaredSubs');
+$bundle->remove_policy('TestingAndDebugging::RequireUseStrict');
+$bundle->remove_policy('TestingAndDebugging::RequireUseWarnings');
+
 #$bundle->remove_policy('ErrorHandling::RequireCarping');
 #$bundle->remove_policy('NamingConventions::Capitalization');
 
