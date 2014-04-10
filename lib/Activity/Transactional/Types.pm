f1a1bde34013ced3e4a390ee772cef52e50f636a
Kent Fredric <kentfredric@gmail.com>
2014-04-11 07:59:56 +1200

    Build results of 460f630 (on master)

--- lib/Activity/Transactional/Types.pm
+++ lib/Activity/Transactional/Types.pm
@@ -2,12 +2,17 @@ package Activity::Transactional::Types;
 $Activity::Transactional::Types::VERSION = '0.001000';
 our $AUTHORITY = 'cpan:ELPENGUIN'; # AUTHORITY
 
+use strict;
+use warnings;
+
 use MooseX::Types -declare => [ qw(Activity Transaction TransactionList) ];
 use MooseX::Types::Moose qw( ArrayRef );
 
 class_type Transaction, { class => 'Activity::Transactional::Transaction' };
 role_type Activity,     { role  => 'Activity::Transactional::Activity' };
 subtype TransactionList, as ArrayRef [Transaction];
+
+1;
 
 __END__
 
